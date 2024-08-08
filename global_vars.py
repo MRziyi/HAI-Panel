e2cdc513c0a6ea4b6c3be46a710344a33537d3e4
“MRziyi” <357408195@qq.com>
2024-08-09 02:33:49 +0800

    v1.0 add config page

--- global_vars.py
+++ global_vars.py
@@ -1,7 +1,13 @@
 from dotenv import load_dotenv
 import os
+import panel as pn
+import autogen
 
 load_dotenv()  # 加载 .env 文件中的所有环境变量
+
+app = None
+app_layout= pn.Column("Modal")
+modal_content = pn.Column("Modal")
 
 chat_interface = None
 chat_status = None
@@ -24,3 +30,10 @@ llm_config={"config_list": [
             "temperature":0, 
             "timeout":3000,
             "seed": 53}
+
+global_assistant= autogen.AssistantAgent(
+            name='Assistant',
+            llm_config=llm_config,
+            human_input_mode="NEVER",
+            system_message='你是Assistant，需要根据用户的要求，参考给出的例子给出json格式的输出',
+        )
